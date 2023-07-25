1fb0babf9ab5ebb46e6a17deb12d06aeb0079aae
Kamran <311.kamran@gmail.com>
2023-07-25 14:32:52 +0400

    IMemoryCache Get() && Set() methods

--- InMemoryApp.Web/Controllers/ProductController.cs
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -1,22 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 
-namespace InMemoryApp.Web.Controllers
+
+namespace InMemoryApp.Web.Controllers;
+
+public class ProductController : Controller
 {
-	public class ProductController : Controller
+	private IMemoryCache _memoryCache;
+
+	public ProductController(IMemoryCache memoryCache)
 	{
-		private IMemoryCache _memoryCache;
-
-		public ProductController(IMemoryCache memoryCache)
-		{
-			_memoryCache = memoryCache;
-		}
+		_memoryCache = memoryCache;
+	}
 
 
 
-        public IActionResult Index()
-		{
-			return View();
-		}
+	public IActionResult Index()
+	{
+		_memoryCache.Set<string>("Zaman", DateTime.Now.ToString());
+		return View();
+	}
+
+
+	public IActionResult Show()
+	{
+		ViewBag.zaman = _memoryCache.Get<string>("Zaman");
+		return View();
 	}
 }
