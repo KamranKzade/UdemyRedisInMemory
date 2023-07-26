f37ccf5e6f91d49648cfbf323f51137341161f5d
Kamran <311.kamran@gmail.com>
2023-07-26 14:04:13 +0400

    IDistributedCache Get(), Set(), Remove() methods DistributerdCacheEntryOptions class

---
+++ IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+
+namespace IDistributedCacheRedisApp.Web.Controllers;
+
+public class ProductsController : Controller
+{
+	// Distributed Cache elave edirik, Redis --> Redis-e sade datalari yazmaq ve oxumaq 
+	private IDistributedCache _distributedCache;
+
+	public ProductsController(IDistributedCache distributedCache)
+	{
+		_distributedCache = distributedCache;
+	}
+
+
+	public async Task<IActionResult> Index()
+	{
+		// Cache optionlar vermek ucun istifade edirik, yasam omru, prioriteti ve s.
+
+		DistributedCacheEntryOptions cacheEntryOptions = new DistributedCacheEntryOptions();
+		cacheEntryOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+
+		// Redis-e data elave edirik.
+		// _distributedCache.SetString("name", "kamran", cacheEntryOptions);
+
+		// Redis-e data elave edirik (asinxron).
+		await _distributedCache.SetStringAsync("surname", "karimzada", cacheEntryOptions);
+
+		return View();
+	}
+
+	public async Task<IActionResult> Show()
+	{
+		// Redis-de olan data elde edirik.
+		// string name = _distributedCache.GetString("name");
+
+		// Redis-de olan data elde edirik(asinxron).
+		string name = await _distributedCache.GetStringAsync("surname");
+
+
+		ViewBag.Name = name;
+		return View();
+	}
+
+	public async Task<IActionResult> Remove()
+	{
+		// Redis - de olan data silirik.
+		// _distributedCache.Remove("name");
+		
+		// Redis - de olan data silirik(asinxron).
+		await _distributedCache.RemoveAsync("surname");
+		return View();
+	}
+}
