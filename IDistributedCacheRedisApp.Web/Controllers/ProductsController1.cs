23664b471ad7edc6aa8257747fefc7acc87675a3
Kamran <311.kamran@gmail.com>
2023-07-26 11:55:42 +0400

    Bolum 4.1 -> Asp.Net Core + Redis ( DistributedCache )

---
+++ IDistributedCacheRedisApp.Web/Controllers/ProductsController1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace IDistributedCacheRedisApp.Web.Controllers;
+
+public class ProductsController1 : Controller
+{
+	// Distributed Cache elave edirik, Redis --> Redis-e sade datalari yazmaq ve oxumaq 
+	private IDistributedCache _distributedCache;
+
+	public ProductsController1(IDistributedCache distributedCache)
+	{
+		_distributedCache = distributedCache;
+	}
+
+
+	public IActionResult Index()
+	{
+		return View();
+	}
+}
