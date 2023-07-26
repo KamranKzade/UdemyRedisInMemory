cd594fe977275e5a895487971e469a6234f26a01
Kamran <311.kamran@gmail.com>
2023-07-26 11:56:54 +0400

    Generate

--- İDistributedCacheRedisApp.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace İDistributedCacheRedisApp.Web.Models
-{
-	public class ErrorViewModel
-	{
-		public string? RequestId { get; set; }
-
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
-}
