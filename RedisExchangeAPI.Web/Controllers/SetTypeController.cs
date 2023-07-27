8714cf218426037a4d8bcbe3cc5b1aa9264b9085
Kamran <311.kamran@gmail.com>
2023-07-27 11:21:34 +0400

    Redis Set Controller -> Generate

--- RedisExchangeAPI.Web/Controllers/SetTypeController.cs
+++ RedisExchangeAPI.Web/Controllers/SetTypeController.cs
@@ -1,56 +1,49 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using RedisExchangeAPI.Web.Services;
-using StackExchange.Redis;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Linq;
+using StackExchange.Redis;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using RedisExchangeAPI.Web.Services;
 
-namespace RedisExchangeAPI.Web.Controllers
+
+namespace RedisExchangeAPI.Web.Controllers;
+
+
+public class SetTypeController : Controller
 {
-	public class SetTypeController : Controller
+	private readonly IDatabase _db;
+	private string listKey = "hashName";
+	private readonly RedisService _redisService;
+
+	public SetTypeController(RedisService redisService)
 	{
-		private readonly IDatabase _db;
-		private string listKey = "hashName";
-		private readonly RedisService _redisService;
-
-
-		public SetTypeController(RedisService redisService)
-		{
-			_redisService = redisService;
-
-			_db = redisService.GetDb(2);
-		}
-
-
-
-
-
-		public IActionResult Index()
-		{
-			HashSet<string> namesList = new HashSet<string>();
-			if (_db.KeyExists(listKey))
-			{
-				_db.SetMembers(listKey).ToList().ForEach(x => namesList.Add(x));
-			}
-
-			return View(namesList);
-		}
-
-		[HttpPost]
-		public IActionResult Add(string name)
-		{
-			_db.KeyExpire(listKey, DateTime.Now.AddMinutes(3));
-			_db.SetAdd(listKey, name);
-
-			return RedirectToAction("index");
-		}
-
-		public async Task<IActionResult> DeleteItem(string name)
-		{
-			await _db.SetRemoveAsync(listKey, name);
-			return RedirectToAction("index");
-		}
-
+		_redisService = redisService;
+		_db = redisService.GetDb(2);
 	}
+
+	public IActionResult Index()
+	{
+		HashSet<string> namesList = new HashSet<string>();
+		if (_db.KeyExists(listKey))
+			_db.SetMembers(listKey).ToList().ForEach(x => namesList.Add(x));
+
+		return View(namesList);
+	}
+
+	[HttpPost]
+	public IActionResult Add(string name)
+	{
+		_db.KeyExpire(listKey, DateTime.Now.AddMinutes(3));
+		_db.SetAdd(listKey, name);
+
+		return RedirectToAction("index");
+	}
+
+	public async Task<IActionResult> DeleteItem(string name)
+	{
+		await _db.SetRemoveAsync(listKey, name);
+		return RedirectToAction("index");
+	}
+
 }
